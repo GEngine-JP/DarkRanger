a1f2a84839083536955c612d0b98bb1ead26f92f
hupeng <xiaomo@xiaomo.info>
2018-10-31 17:58:38 +0800

    通过角色控制器控制角色的移动

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+*  作   者 ：胡朋
+*  github : https://github.com/xiaomoinfo
+*  描   述 ：
+*/
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    private CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        cc.SimpleMove(new Vector3(x * moveSpeed, 0, z * moveSpeed));
+    }
+}
